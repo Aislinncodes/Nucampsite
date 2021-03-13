1b1eae2d096809727543c24acca41ec843ef3e72
Aislinn Wallace Hendricks <aislinnwh@gmail.com>
2021-03-13 14:23:19 -0500

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 $(function() {
   $(".carousel").carousel( { interval: 2000, pause: "false" } );
-  $("#carouselButton").click(function(){
+  $("#carouselButton").on('click', function(){
     if ($("#carouselButton").children("i").hasClass("fa-pause")) {
             $(".carousel").carousel("pause");
             $("#carouselButton").children("i").removeClass("fa-pause");
@@ -11,4 +11,15 @@ $(function() {
             $("#carouselButton").children("i").addClass("fa-pause"); 
         }
   });
+
+  $('#rsvpButton').on('click', function() {
+    $('#reserveModal').modal("show");
+  });
+
+  $('#loginButton').on('click', function(){
+    $('#loginModal').modal("show");
+  });
 });
+
+
+
